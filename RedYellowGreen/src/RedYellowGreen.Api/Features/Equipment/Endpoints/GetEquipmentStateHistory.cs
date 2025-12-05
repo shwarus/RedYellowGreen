6d4da89509159ae2c079af088e8dfe6bf61af1a9
Lukas Sinkus <shwarus@gmail.com>
2025-12-05 14:22:22 +0100

    GetEquipmentStateHistory

---
+++ RedYellowGreen/src/RedYellowGreen.Api/Features/Equipment/Endpoints/GetEquipmentStateHistory.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RedYellowGreen.Api.Features.Equipment.Models;
+using RedYellowGreen.Api.Infrastructure.Database;
+
+namespace RedYellowGreen.Api.Features.Equipment.Endpoints;
+
+public class GetEquipmentStateHistory : BaseEquipmentController
+{
+    public record Result(
+        EquipmentState State,
+        DateTime CreatedAt
+    );
+
+    [HttpGet("{equipmentId:guid}/state-history")]
+    public async Task<Result[]> Handle(
+        [FromServices] AppDbContext context,
+        [FromRoute] Guid equipmentId
+    ) =>
+        await context.Equipment
+            .Where(equipment => equipment.Id == equipmentId)
+            .Select(equipment => equipment.States.Select(state => new Result(state.State, state.CreatedAt)).ToArray())
+            .SingleAsync();
+}
