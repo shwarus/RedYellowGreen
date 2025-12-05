70a30c6c636095c8bcf13244f574802972350736
Lukas Sinkus <shwarus@gmail.com>
2025-12-05 14:18:29 +0100

    GetSupervisorViewEquipment

---
+++ RedYellowGreen/src/RedYellowGreen.Api/Features/Equipment/Endpoints/GetSupervisorViewEquipment.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RedYellowGreen.Api.Features.Equipment.Models;
+using RedYellowGreen.Api.Infrastructure.Database;
+
+namespace RedYellowGreen.Api.Features.Equipment.Endpoints;
+
+public class GetSupervisorViewEquipment : BaseEquipmentController
+{
+    public record Result(
+        Guid Id,
+        string Title,
+        EquipmentState State
+    );
+
+    [HttpGet("supervisor-view")]
+    public async Task<Result[]> Handle(
+        [FromServices] AppDbContext context
+    ) =>
+        await context.Equipment
+            .Select(equipment => new Result(equipment.Id, equipment.Title, equipment.CurrentState.State))
+            .ToArrayAsync();
+}
